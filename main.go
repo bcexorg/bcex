ee6c09b1ffbb4428691e31e5e6d34b06204c4361
Biomass Exchange Co., Ltd <bcexorg@gmail.com>
2021-10-25 22:15:27 +0700

    platform added

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func (ih *indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		 		<title>BCEX</title>
 	    	</head>
 			<body>
-				<b>Biomass Commodity Exchange</b>
+				<b>Biomass Commodity Exchange Platform</b>
         		<p>
           		<a href="/menu1">page 1</a> | <a href="/menu2">page 2</a> | <a href="/menu3">page 3</a> | <a href="/menu4">page 4</a>
         		</p>
